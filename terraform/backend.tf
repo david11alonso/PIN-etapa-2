bc9146a4d171ee658cc4c395a1802934dbb25f77
david11alonso <74559529+david11alonso@users.noreply.github.com>
2025-02-01 19:42:01 -0600

    Cambio formato

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3"{
-    bucket                 = "mundoepin2"
-    region                 = "us-east-1"
-    key                    = "backend.tfstate"
+  backend "s3" {
+    bucket = "mundoepin2"
+    region = "us-east-1"
+    key    = "backend.tfstate"
   }
 }
